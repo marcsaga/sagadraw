10e6ed59f7f90180c209e6d8ecd7ccb8d5357ebc
marc <marcsagabcn@gmail.com>
2023-08-26 02:17:24 +0200

    add delete listener test

---
+++ src/tests/delete-listener.test.tsx
@@ -0,0 +1,43 @@
+import { fireEvent, render } from "@testing-library/react";
+import { Canvas } from "~/canvas/canvas";
+import { CanvasElementStorage } from "~/canvas/storage/canvas-element-storage";
+import { mockRectangle } from "./mockers/elements";
+
+const selectedMockedRect = mockRectangle({ selected: true });
+const unselectedMockedRect = mockRectangle({ selected: false });
+
+describe("delete listener", () => {
+  afterEach(() => {
+    CanvasElementStorage.clear();
+  });
+
+  it("should be able to delete a selected element", () => {
+    CanvasElementStorage.set([selectedMockedRect]);
+
+    const { container } = render(<Canvas />);
+    const canvas = container.querySelector("#canvas")!;
+
+    fireEvent.keyDown(canvas, { key: "Backspace" });
+    expect(CanvasElementStorage.get()).toEqual([]);
+  });
+
+  it("should not be able to delete an unselected element", () => {
+    CanvasElementStorage.set([unselectedMockedRect]);
+
+    const { container } = render(<Canvas />);
+    const canvas = container.querySelector("#canvas")!;
+
+    fireEvent.keyDown(canvas, { key: "Backspace" });
+    expect(CanvasElementStorage.get()).toEqual([unselectedMockedRect]);
+  });
+
+  it("should be able to delete more than one selected element at the same time", () => {
+    CanvasElementStorage.set([selectedMockedRect, selectedMockedRect]);
+
+    const { container } = render(<Canvas />);
+    const canvas = container.querySelector("#canvas")!;
+
+    fireEvent.keyDown(canvas, { key: "Backspace" });
+    expect(CanvasElementStorage.get()).toEqual([]);
+  });
+});
